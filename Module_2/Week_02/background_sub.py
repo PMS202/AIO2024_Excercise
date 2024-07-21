134d31fd5dc6ba4e39cd1eeaec05ac55bd598689
PMS202 <misalol2510@gmail.com>
2024-07-21 22:29:51 +0700

    Nop bai tap module 2 week 02

--- Module_2/Week_02/background_sub.py
+++ Module_2/Week_02/background_sub.py
@@ -2,6 +2,24 @@ import cv2
 import numpy as np
 
 
+def compute_differenc(background,  image):
+    dif = cv2.absdiff(background, image)
+    dif_mono = np.sum(dif, axis=2) / 3.0
+    dif_mono = dif_mono.astype('uint8')
+    return dif_mono
+
+
+def compute_binary_mask(dif_mono):
+    _, mask = cv2.threshold(dif_mono, 15, 255, cv2.THRESH_BINARY)
+    mask = np.stack((mask, mask, mask), axis=-1)
+    return mask
+
+
+def replace_background(mask, background_sub, image):
+    image_sub = np.where(mask == 0, background_sub, image)
+    return image_sub
+
+
 def background_subtraction(background_dir, background_sub_dir, image_dir):
     IMAGE_SIZE = (678, 381)
     background = cv2.imread(background_dir)
@@ -12,15 +30,9 @@ def background_subtraction(background_dir, background_sub_dir, image_dir):
 
     image = cv2.imread(image_dir)
     image = cv2.resize(image, IMAGE_SIZE)
-
-    dif = cv2.absdiff(background, image)
-    dif_mono = np.sum(dif, axis=2) / 3.0
-    dif_mono = dif_mono.astype('uint8')
-
-    _, mask = cv2.threshold(dif_mono, 15, 255, cv2.THRESH_BINARY)
-    mask = np.stack((mask, mask, mask), axis=-1)
-
-    image_sub = np.where(mask == 0, background_sub, image)
+    dif_mono = compute_differenc(background,  image)
+    mask = compute_binary_mask(dif_mono)
+    image_sub = replace_background(mask, background_sub, image)
 
     while True:
         cv2.imshow('Khung hình', image_sub)
