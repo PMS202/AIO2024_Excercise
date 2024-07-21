28d2b0b2b2106a20ab5fb7cb5c376d30afce4a80
PMS202 <misalol2510@gmail.com>
2024-07-21 22:19:24 +0700

    Nop bai tap module 2 week 02

---
+++ Module_2/Week_02/background_sub.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+
+
+def background_subtraction(background_dir, background_sub_dir, image_dir):
+    IMAGE_SIZE = (678, 381)
+    background = cv2.imread(background_dir)
+    background = cv2.resize(background, IMAGE_SIZE)
+
+    background_sub = cv2.imread(background_sub_dir)
+    background_sub = cv2.resize(background_sub, IMAGE_SIZE)
+
+    image = cv2.imread(image_dir)
+    image = cv2.resize(image, IMAGE_SIZE)
+
+    dif = cv2.absdiff(background, image)
+    dif_mono = np.sum(dif, axis=2) / 3.0
+    dif_mono = dif_mono.astype('uint8')
+
+    _, mask = cv2.threshold(dif_mono, 15, 255, cv2.THRESH_BINARY)
+    mask = np.stack((mask, mask, mask), axis=-1)
+
+    image_sub = np.where(mask == 0, background_sub, image)
+
+    while True:
+        cv2.imshow('Khung hình', image_sub)
+        if cv2.waitKey(1) & 0xFF == ord('e'):
+            break
+
+    # Giải phóng bộ nhớ
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    background_dir = r"D:\Hoc\AIO2024\AIO2024_Excercise\Module_2\Week_02\Image_data\GreenBackground.png"
+    background_sub_dir = r"D:\Hoc\AIO2024\AIO2024_Excercise\Module_2\Week_02\Image_data\NewBackground.jpg"
+    image_dir = r"D:\Hoc\AIO2024\AIO2024_Excercise\Module_2\Week_02\Image_data\Object.png"
+    background_subtraction(background_dir, background_sub_dir, image_dir)
